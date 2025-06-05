74ba4e5a49d602adda120134fdce6d821b588806
Aditya Choudhary <caditya965@gmail.com>
2025-06-05 15:20:16 +0530

    added 04_forloop.cpp

---
+++ C++/04_forloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cout<<"Enter any no.: ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
