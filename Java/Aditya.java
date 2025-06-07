e9ca232db565c7f29d8f791b14d4762a9335e75d
Aditya Choudhary <caditya965@gmail.com>
2025-06-08 00:44:57 +0530

    Updated Java programs

--- Java/Aditya.java
+++ Java/Aditya.java
@@ -3,11 +3,15 @@ class Aditya
 	public static void main(String args[])
 	{
 		float p,r,t,si;
-		System.out.println("Jay Shree Ram");
+		System.out.println("Enter command line arguments: as java Aditya P R T");
 		p=Float.parseFloat(args[0]);
 		r=Float.parseFloat(args[1]);
 		t=Float.parseFloat(args[2]);
 		si=(p*r*t)/100;
+		System.out.println("Principle= Rs"+ p);
+		System.out.println("Rate= "+ r+" %");
+		System.out.println("Time= "+ t+ " Years");
+
 		System.out.println("Simple interest= "+ si);
 	}
 }
