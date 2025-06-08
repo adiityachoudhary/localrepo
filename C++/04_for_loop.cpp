1a3029b63cd5c132ea75817d1714447884fb021c
Aditya Choudhary <caditya965@gmail.com>
2025-06-08 23:23:15 +0530

    added new files 04_loop and 04_for_loop which prints n times 'Birthday' text by both loops

---
+++ C++/04_for_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "How many do you want to print Happy Birthday?\n";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cout << "Happy Birthday Aniket:)\n";
+
+    return 0;
+}
---
+++ C++/04_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int n, i=0;
+    cout << "How many do you want to print Happy Birthday?\n";
+    cin >> n;
+
+    while (i < n)
+    {
+        cout << "Happy Birthday Aniket:)\n";
+        i++;
+    }
+    return 0;
+}
