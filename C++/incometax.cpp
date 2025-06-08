8724a2e9f4b108b1ab56fcbac1fbadbfd3674a15
Aditya Choudhary <caditya965@gmail.com>
2025-06-08 22:18:37 +0530

    Improved logic of incometax.cpp, leapyear.cpp, and capitalString.cpp

--- C++/incometax.cpp
+++ C++/incometax.cpp
@@ -6,30 +6,31 @@ int main()
 {
     system("cls");
     int income;
-    float tax;
+    double tax = 0;
     cout << "Enter your annual income: ";
     cin >> income;
 
-    if (income < 250000)
-        cout << "You don't have to pay any income tax";
+    // there is not tax for income of 2.5 lacs
+    if (income <= 250000)
+        tax = 0;
 
-    else if (income >= 250000 && income < 500000)
+    else if (income > 250000 && income < 500000) // for the first 2.5 lacs there is no tax but on the rest amount
     {
-        tax = income * 0.05;
-        cout << "You have to pay Rs " << tax << " as income tax";
+        tax = (income - 250000) * 0.05;
     }
 
-    else if (income >= 500000 && income < 1000000)
+    else if (income >= 500000 && income < 1000000) // for the first 2.5 lacs there is no tax but next 2.5L -> 5%, next 5L ->20%
     {
-        tax = income * 0.20;
-        cout << "You have to pay Rs " << tax << " as income tax";
+        tax = (250000 * 0.05) + (income - 500000) * 0.20;
     }
 
+    // for the first 2.5 lacs there is no tax but on next 2.5L -> 5%, next 5L ->20% and the rest amount is charged 30%
     else if (income >= 1000000)
     {
-        tax = income * 0.30;
-        cout << "You have to pay Rs " << tax << " as income tax";
+        tax = (250000 * 0.05) + (500000 * 0.20) + (income - 1000000) * 0.30;
     }
+    cout << "You have to pay Rs " << tax << " as income tax";
+
     cin.get();
     return 0;
 }
--- C++/leapyear.cpp
+++ C++/leapyear.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main()
 {
     system("cls");
-    int year, l;
+    int year;
     cout << "Enter year: ";
     cin >> year;
-    l = year % 4;
-    if (l == 0)
-        cout << year << " is a Leap year";
-        else 
-        cout << year << " is not a Leap year";
+
+    // if an year is divisible by 4 and 400 but not by 100 it is a leap year
+
+    bool l = (year % 4 == 0) && (year % 400 == 0 || year % 100 != 0);
+
+    if (l)
+        cout << year << " is a Leap year"; // if L is TRUE
+    else
+        cout << year << " is not a Leap year"; // if L is FALSE
 
     return 0;
 }
