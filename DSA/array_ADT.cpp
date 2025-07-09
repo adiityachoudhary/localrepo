997078a26e83c129ca9768a7d8ab3614f937b985
Aditya Choudhary <caditya965@gmail.com>
2025-07-10 01:42:20 +0530

    embedded resume and added icons

--- DSA/array_ADT.cpp
+++ DSA/array_ADT.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
 
-struct Arr
+struct myArray
 {
+    int total_size;
+    int used_size;
     int *ptr;
-    ptr = (int *)malloc(5 * sizeof(int));
 };
+
 
 int main()
 {
-    struct Arr a[5];
-    a[0] = 1;
-    a[1] = 1;
-    a[2] = 1;
-    a[3] = 1;
+    struct myArray a;
 
+   
     return 0;
 }
