37f69ede515481e14a762763c662e88c281d0a27
Aditya Choudhary <caditya965@gmail.com>
2025-07-10 12:01:15 +0530

    array ADT

--- DSA/array_ADT.cpp
+++ DSA/array_ADT.cpp
@@ -7,12 +7,28 @@ struct myArray
     int used_size;
     int *ptr;
 };
-
+void create_array(struct myArray *a, int tsize, int usize)
+{
+    (*a).total_size = tsize;
+    (*a).used_size = usize;
+    (*a).ptr = (int *)malloc(tsize * sizeof(int));
+}
 
 int main()
 {
-    struct myArray a;
+    struct myArray marks;
+    create_array(&marks, 10, 5);
+    
+    marks.ptr[0] = 25;
+    marks.ptr[1] = 55;
+    marks.ptr[2] = 75;
+    marks.ptr[3] = 45;
+    marks.ptr[4] = 35;
 
-   
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<marks.ptr[i]<<endl;
+    }
+    
     return 0;
 }
