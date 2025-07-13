aba554da9f54b4628bd2a6905e821b3fe3ee6c40
Aditya Choudhary <caditya965@gmail.com>
2025-07-14 00:35:11 +0530

    backtracking and recursion lecture started

---
+++ DSA/Recursion_Backtracking/print_no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void printNo(int n)
+{
+    if (n == 1) // base case
+    {
+        cout << "1 ";
+        return;
+    }
+    cout << n << ", ";
+    return printNo(n - 1); // recursive function
+}
+
+int main()
+{
+    printNo(10);
+    return 0;
+}
